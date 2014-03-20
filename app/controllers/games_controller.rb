9e805fc4147919b4666eac16dbb539a09096ed5c
John Hinrichs <jmhinric@gmail.com>
2014-03-20 14:00:19 -0400

    working on round

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,12 @@
 class GamesController < ApplicationController
 
-  def new
+  def create
     @game = Game.create
     redirect_to game_path(@game)
   end
 
   def show
-
+    @game = Game.find_by_id(params[:id])
+    @category_lists = @game.round.category_list[:list_one]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Scattergories::Application.routes.draw do
   root to: "welcome#index"
 
-  resources :games, only: [:new, :show]
+  resources :games, only: [:new, :create, :show]
   # get "games/:game_number", to: "games#show"
 end
