6496bedc46f51959fec47a8986fa91db73b60df4
John Hinrichs <jmhinric@gmail.com>
2014-03-20 14:20:02 -0400

    the category list syncs with the play card slots

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,10 @@
 class Game < ActiveRecord::Base
   has_many :rounds
-  #   "8q324iorjesa8fd"
-    # numbers = (0..9).to_a
-    # letters = (a..f).to_a
-    # hex = numbers + letters
-  # end
+
+  def category_list
+    @category_lists = Hash.new
+    @category_lists[:list_one] = ["A boy's name", "A river", "An animal", "Things that are cold", "Insects", "TV Shows", "Things that grow", "Fruits", "Things that are black", "School subjects", "Movie Titles", "Musical Instruments"]
+    return @category_lists
+  end
 
 end
