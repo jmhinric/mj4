2cc6075ae934dda4f96d2859abfb4998a54d1d33
John Hinrichs <jmhinric@gmail.com>
2014-03-20 14:56:11 -0400

    Add timer and time's up actions

--- app/assets/javascripts/round.js
+++ app/assets/javascripts/round.js
@@ -1,15 +1,38 @@
 $(document).ready(function () {
+  var intervalId;
+  var time = 3;
+  // var categoryLists = {};
+  // categoryLists["listOne"] = ["A boy's name", "A river", "An animal", "Things that are cold", "Insects", "TV Shows", "Things that grow", "Fruits", "Things that are black", "School subjects", "Movie Titles", "Musical Instruments"];
 
-  var categoryLists = {};
-  categoryLists["listOne"] = ["A boy's name", "A river", "An animal", "Things that are cold", "Insects", "TV Shows", "Things that grow", "Fruits", "Things that are black", "School subjects", "Movie Titles", "Musical Instruments"];
 
 
-
-  function createCategories() {
-    $.each(categoryLists["listOne"], function(index, category) {
-      $("<li>").text(category).appendTo(".category-names");
+  // function createCategories() {
+  //   $.each(categoryLists["listOne"], function(index, category) {
+  //     $("<li>").text(category).appendTo(".category-names");
+  //   });
+  // }
+  // createCategories();
+  var timer = $(".timer button");
+  timer.one("click", function() {
+    intervalId = setInterval(countDown, 1000);
     });
+
+  function countDown() {
+    time -= 1;
+    
+    if (time === 0) {
+      timer.text(":0" + time);
+      timeUp();
+    } else if(time < 10) {
+      timer.text(":0" + time);
+    } else {
+      timer.text(":" + time);
+    }
   }
 
-  createCategories();
-}
+  function timeUp() {
+    clearInterval(intervalId);
+    $("header").text("Time's Up!!!");
+    $(".playcard").attr("disabled", "disabled");
+  }
+});
