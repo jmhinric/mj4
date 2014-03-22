6c9f0635123326d28296b3b275aff405a740f004
John Hinrichs <jmhinric@gmail.com>
2014-03-22 18:13:53 -0400

    add js model tests and functions for scoring

--- app/assets/javascripts/round.js
+++ app/assets/javascripts/round.js
@@ -1,71 +1,3 @@
-
-function Round(){
-  this.letter = "";
-  this.answers = [];
-  this.answersObject = {};
-  this.scores = [];
-}
-
-Round.prototype.setLetter = function( ){
-  $.ajax({
-    dataType: "json",
-    url: "letter",
-    data: {id: window.location.pathname.replace("/rounds/", "")},
-    success: function(success) {
-      $("#roll_result").text(success.letter);
-      round.letter = success.letter;
-    }
-  });
-};
-
-// Function to take the User's answers and:
-//    1)  Score blank answers as 0
-//    2)  Score answers that don't start with the proper letter as 0
-Round.prototype.autoRejectAnswers = function() {
-  $.ajax({
-    dataType: "json",
-    url: "auto_reject",
-    data: {answers: this.answersObject, id: window.location.pathname.replace("/rounds/", "")},
-    success: function(success) {
-      console.log(success);
-      for (var j = 0; j < 12; j++) {
-        round.scores[j] = success["scores"][j];
-      }
-    }
-  });
-};
-
-// Function to take the User's answers from the input fields and store them in the Round constructor function's answers array
-Round.prototype.getAnswers = function() {
-  for(var i = 0; i < 12; i++) {
-    this.answers.push($('#answer-' + (i+1)).val());
-    this.answersObject[i] = $('#answer-' + (i+1)).val();
-  }
-  console.log(this.answers);
-  console.log(this.answersObject);
-};
-
-
-// Round.prototype.render = function(){
-//   // grab an element on the DOM
-//   // create all of the elements for the round
-//   // hook these elements on to the DOM via the first element you grabbed
-//   $();
-// };
-
-// Round.prototype.synchronize = function(){
-//  $.ajax();
-// };
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function () {
 
   var intervalId;
@@ -87,22 +19,10 @@ $(document).ready(function () {
   // createCategories();
 
   // var buttonPress = $("#die_button");
+  
   var randomLetterButton = $("#die_button");
-
   randomLetterButton.on("click", function() {
-    timer.attr("disabled", false);
-    randomLetterButton.attr("disabled", true);
     round.setLetter();
-    // var letter = $.ajax({
-    //   dataType: "json",
-    //   url: "letter",
-    //   data: {id: window.location.pathname.replace("/rounds/", "")},
-    //   success: function(success) {
-    //     $("#roll_result").text(success.letter);
-    //     round.letter = success.letter;
-    //   }
-    // });
-
   });
 
 
@@ -132,79 +52,201 @@ $(document).ready(function () {
     $(".playcard").attr("disabled", "disabled");
     round.getAnswers();
     round.autoRejectAnswers();
-    usersJudgeAnswers();
-    rejectBadAnswers();
-  }
-
-  function usersJudgeAnswers() {
-
-    for(var i = 1; i < 13; i++) {
-      var button = $("<button>").text("Reject");
-      button.addClass("reject-button");
-      button.attr("id", "reject-" + i);
-
-      var id = "#slot-" + i;
-      button.appendTo(id); 
-      
-      $(button).on("click", function() {
-        $(this).toggleClass("rejected-button");
-        $(this).siblings().toggleClass("rejected-input");
-        updateScore();
-      });
-    }
-    finishScoring();
   }
 
 
 // Use some logic and code from this function for the Ruby Round class auto_reject method
-  function rejectBadAnswers() {
-    for(var i = 1; i < 13; i++) {
-      var randomLetter = $("#roll_result").text();
-      if ( $('#answer-' + i).val() == "" ) {
-        $('#reject-' + i).addClass("rejected-button");
-        $('#reject-' + i).siblings().addClass("rejected-input");
-        $('#reject-' + i).attr("disabled", "disabled");
-        answerPoints[i - 1] = 0;
-      } else if ( $('#answer-' + i).val().charAt(0).toLowerCase() !== randomLetter ) {
-        $('#reject-' + i).addClass("rejected-button");
-        $('#reject-' + i).siblings().addClass("rejected-input");
-        $('#reject-' + i).attr("disabled", "disabled");
-        answerPoints[i - 1] = 0;
-      } else {
-        answerPoints[i - 1] = 1;
-      }
+  // function rejectBadAnswers() {
+  //   for(var i = 1; i < 13; i++) {
+  //     var randomLetter = $("#roll_result").text();
+  //     if ( $('#answer-' + i).val() == "" ) {
+  //       $('#reject-' + i).addClass("rejected-button");
+  //       $('#reject-' + i).siblings().addClass("rejected-input");
+  //       $('#reject-' + i).attr("disabled", "disabled");
+  //       answerPoints[i - 1] = 0;
+  //     } else if ( $('#answer-' + i).val().charAt(0).toLowerCase() !== randomLetter ) {
+  //       $('#reject-' + i).addClass("rejected-button");
+  //       $('#reject-' + i).siblings().addClass("rejected-input");
+  //       $('#reject-' + i).attr("disabled", "disabled");
+  //       answerPoints[i - 1] = 0;
+  //     } else {
+  //       answerPoints[i - 1] = 1;
+  //     }
 
-      updateScore();
-    }
-  }
+  //     updateScore();
+  //   }
+  // }
 
  
 
-  function updateScore() {
-    var rejected = $(".rejected-button").length;
-    finalScore = 12 - rejected;
+  // function updateScore() {
+  //   var rejected = $(".rejected-button").length;
+  //   finalScore = 12 - rejected;
 
-  }
+  // }
 
-  function finishScoring() {
-    var finishButton = $("<button id='submit-scores'>");
-    finishButton.addClass("finish-button");
-    finishButton.text("Finished Scoring");
-    finishButton.one("click", endGame);
-    finishButton.appendTo(".score");
-
-  }
-
-  function endGame() {
-    $("<h1>").text("Game Over!").appendTo(".score");
-    $("<h2>").text("Final Score: " + finalScore).appendTo(".score");
-    $(".reject-button").attr("disabled", true);
-    $(".finish-button").remove();
-    $(".score h3").remove();
-  }
+  
 
 
   
 
 
 });
+
+
+function Round(){
+  this.letter = "";
+  this.answers = [];
+  this.answersObject = {};
+  this.scores = [];
+  this.scoresObject = {};
+  this.finalScore = 0;
+}
+
+Round.prototype.setLetter = function( ){
+  $.ajax({
+    dataType: "json",
+    url: "letter",
+    data: {id: window.location.pathname.replace("/rounds/", "")},
+    success: function(success) {
+      $(".timer button").attr("disabled", false);
+      $("#die_button").attr("disabled", true);
+      $("#roll_result").text(success.letter);
+      round.letter = success.letter;
+    }
+  });
+};
+
+// Function to take the User's answers from the input fields and store them in the Round constructor function's answers array
+Round.prototype.getAnswers = function() {
+  for(var i = 0; i < 12; i++) {
+    this.answers.push($('#answer-' + (i+1)).val());
+    this.answersObject[i] = $('#answer-' + (i+1)).val();
+  }
+  console.log(this.answers);
+  console.log(this.answersObject);
+};
+
+
+// Function to take the User's answers and:
+//    1)  Score blank answers as 0
+//    2)  Score answers that don't start with the proper letter as 0
+Round.prototype.autoRejectAnswers = function() {
+  $.ajax({
+    dataType: "json",
+    url: "auto_reject",
+    data: {answers: this.answersObject, id: window.location.pathname.replace("/rounds/", "")},
+    success: function(success) {
+      console.log(success);
+      for (var j = 0; j < 12; j++) {
+        round.scores[j] = success["scores"][j];
+      }
+      round.usersJudgeAnswers();
+      round.updateRejectedStyles();
+      round.finishScoring();
+      // rejectBadAnswers();
+    }
+  });
+};
+
+Round.prototype.usersJudgeAnswers = function() {
+
+  for(var i = 1; i < 13; i++) {
+    var button = $("<button>").text("Reject");
+    button.addClass("reject-button");
+    button.attr("id", "reject-" + i);
+
+    var id = "#slot-" + i;
+    button.appendTo(id);
+    
+    $(button).on("click", function() {
+      $(this).toggleClass("rejected-button");
+      $(this).siblings().toggleClass("rejected-input");
+      // updateScore();
+    });
+  }
+};
+
+
+// Add appropriate CSS styles to auto-rejected buttons and inputs before the User voting round
+Round.prototype.updateRejectedStyles = function() {
+  for (var j = 0; j < 12; j++) {
+    var buttonId = "#reject-" + (j+1);
+    if(this.scores[j] === 0) {
+      $(buttonId).toggleClass("rejected-button");
+      $(buttonId).siblings().toggleClass("rejected-input");
+    }
+  }
+};
+
+
+Round.prototype.finishScoring = function() {
+  var finishButton = $("<button id='submit-scores'>");
+  finishButton.addClass("finish-button");
+  finishButton.text("Finished Scoring");
+  finishButton.one("click", this.endGame);
+  finishButton.appendTo(".score");
+
+};
+
+Round.prototype.endGame = function() {
+  this.getUserVotes();
+  this.setFinalScore();
+  $("<h1>").text("Game Over!").appendTo(".score");
+  $("<h2>").text("Final Score: " + this.finalScore).appendTo(".score");
+  $(".reject-button").attr("disabled", true);
+  $(".finish-button").remove();
+  $(".score h3").remove();
+};
+
+// Function to see which answers the user rejected
+Round.prototype.getUserVotes = function() {
+  for(var i = 0; i < 12; i++) {
+    var buttonId = "#reject-" + i;
+    if($(buttonId).class === "rejected-button") {
+      this.scores[i] = 0;
+      this.scoresObject[i] = 0;
+    } else {
+      this.scores[i] = 1;
+      this.scoresObject[i] = 1;
+    }
+  }
+};
+
+Round.prototype.setFinalScore = function() {
+  $.ajax({
+    dataType: "json",
+    url: "auto_reject",
+    data: {answers: this.answersObject, id: window.location.pathname.replace("/rounds/", "")},
+    success: function(success) {
+      console.log(success);
+      for (var j = 0; j < 12; j++) {
+        round.scores[j] = success["scores"][j];
+      }
+      // round.usersJudgeAnswers();
+      // rejectBadAnswers();
+    }
+  });
+};
+
+
+// Round.prototype.render = function(){
+//   // grab an element on the DOM
+//   // create all of the elements for the round
+//   // hook these elements on to the DOM via the first element you grabbed
+//   $();
+// };
+
+// Round.prototype.synchronize = function(){
+//  $.ajax();
+// };
+
+
+
+
+
+
+
+
+
+
--- spec/javascripts/round_spec.js
+++ spec/javascripts/round_spec.js
@@ -8,24 +8,43 @@ describe("Round", function(){
     round = new Round();
   });
 
-  // describe("setLetter", function(){
-  //   it("stores a letter for the round", function(){
-  //     round.setLetter("A");
-  //     expect(round.letter).toBe("A");
-  //   });
-  // });
+  describe("setLetter", function(){
+    it("stores a letter for the round", function(){
+      round.setLetter();
+      expect(round.letter).toBe("A");
+    });
+  });
 
   describe("autoRejectAnswers", function() {
-    it("scores every slot in a playcard", function() {
+    var value, async = new AsyncSpec(this);
+
+    // async.beforeEach(function(done) {
+    //   value = 0;
+    //   setTimeout(function() {
+    //     value++;
+      
+    //     done();
+    //   }, 500);
+    // });
+    
+    // beforeEach(function(done) {
+    //   setTimeout(function() {
+    //     value = 0;
+    //     done();
+    //   }, 5000);
+    // });
+
+    it("scores empty slots or words that don't start with the round's letter as 0", function() {
+      // done();
       round.setLetter();
       round.answersObject = {
-        0: "a",
+        0: round.letter,
         1: "a",
         2: "",
-        3: "",
-        4: "a",
-        5: "a",
-        6: "a",
+        3: round.letter,
+        4: "",
+        5: round.letter,
+        6: "",
         7: "a",
         8: "",
         9: "a",
@@ -37,17 +56,20 @@ describe("Round", function(){
       expect(sum(round.scores)).toEqual(3);
     });
 
-    // it("rejects answers that are blank", function() {
-    //   round.autoRejectAnswers();
-    //   expect(round.scores.length).toBe(12);
-
-    // });
   });
 
   describe("getAnswers", function() {
     it("takes answers from the input fields and saves them in an array", function() {
+      round.answers = [];
       round.getAnswers();
       expect(round.answers.length).toBe(12);
+    });
+  });
+
+  describe("setFinalScore", function() {
+    it("finds the end result of which answers should be rejected", function() {
+      round.
+      expect(round.)
     });
   });
 
